e2e12265af8f15df4db4071e7a13ac452c3689d2
Tyson Cruz <32321720+TMCRUZ24@users.noreply.github.com>
2019-06-11 22:08:27 -0700

    Week 1 Gameboard

---
+++ Course Project/Course Project/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course_Project
+{
+    class Program{
+        static void Main(string[] args){
+
+            Console.WriteLine("Enter the number of rows: ");
+            String input = Console.ReadLine();
+            int numOfRows;
+            Int32.TryParse(input, out numOfRows);
+
+            Console.WriteLine("Enter number of columns: ");
+            input = Console.ReadLine();
+            int numOfColumns;
+            Int32.TryParse(input, out numOfColumns);
+
+            gameboardModel myGameboard = new gameboardModel(numOfRows, numOfColumns);
+            myGameboard.setGameBoard();
+            myGameboard.displayGameboard();
+
+        }
+    }
+}
